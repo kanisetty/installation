703818fa1876bec468739820e47ca6aa5e1d1fb8
harsha <harsha@harsh.com>
2017-11-22 10:55:32 +0530

    Add'

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'installation'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures installation'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.1.0'
